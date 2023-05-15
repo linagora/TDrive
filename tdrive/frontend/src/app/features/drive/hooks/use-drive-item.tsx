2156c398ad754dd650af59c97a83240b80532d9c
Romaric Mourgues <rmourgues@linagora.com>
2023-05-15 12:00:56 +0200

    Implemented password and expiration date on conf and backend side

--- tdrive/frontend/src/app/features/drive/hooks/use-drive-item.tsx
+++ tdrive/frontend/src/app/features/drive/hooks/use-drive-item.tsx
@@ -97,7 +97,8 @@ export const getPublicLink = (item?: DriveItem): string => {
     publicLink +=
       `/shared/${translator.fromUUID(item?.company_id || '')}` +
       `/drive/${translator.fromUUID(item?.id || '')}` +
-      `/t/${item?.access_info?.public?.token}`;
+      `/t/${item?.access_info?.public?.token}` +
+      (item?.access_info?.public?.password ? '&pwd' : '');
   } catch (e) {
     return publicLink;
   }

ca14f5c6f26ef2e5a7457fb870cc80f93c1e7e08
Esteban DECTOT <estebandectot@gmail.Com>
2023-07-07 16:27:38 +0200

    Add bassis, adaptation of draggables

--- tdrive/frontend/src/app/features/dragndrop/hook/draggable.tsx
+++ tdrive/frontend/src/app/features/dragndrop/hook/draggable.tsx
@@ -16,8 +16,8 @@ export function Draggable(props:DraggableProps) {
 
   
   return (
-    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
       {props.children}
-    </button>
+    </div>
   );
 }
